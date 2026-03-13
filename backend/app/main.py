799c42f4dc76dc5ed010557bcaa7176bd6e3c252
dharani-izonetech <dharani.izonetech@gmail.com>
2026-03-13 08:29:57 +0530

    added origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="DMK Campaign Coordination Platform", version="2.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://tweet.risingsuntech.in"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
